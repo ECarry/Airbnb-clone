797a394b11ff63986208a1a88c8bc0ca5ebca6a6
ecarry <lianshiliang93@gmail.com>
2023-05-09 15:06:40 +0800

    feat: Listing card component, direct server action

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import { Nunito } from 'next/font/google'
 
+import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
+
 import './globals.css'
+
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
 import RegisterModal from './components/modals/RegisterModal'
-import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
-import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
 
 export const metadata = {
@@ -35,7 +37,9 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
       </body>
     </html>
   )
